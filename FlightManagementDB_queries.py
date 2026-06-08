5fe405ff4e042fd8aa704d570781e785b48d330a
roblowe84 <81348921+roblowe84@users.noreply.github.com>
2026-06-08 06:24:57 +0000

    Include Add New Flight Functionality

--- FlightManagementDB_queries.py
+++ FlightManagementDB_queries.py
@@ -1,11 +1,14 @@
 import sqlite3
 
+
 def view_flights(cursor):
+    # View all flights and their current status as per the database
     query = '''SELECT FlightID, FlightStatus FROM Flight'''
     cursor.execute(query)
     return cursor.fetchall()
 
 def view_pilotschedule(cursor, viewpilotID):
+    # View the current pilots schedule based on PilotID
     query = '''
         SELECT 
             Flight.FlightID,
@@ -21,12 +24,19 @@ def view_pilotschedule(cursor, viewpilotID):
     return cursor.fetchall()
 
 def return_allairports(cursor):
+    # Return all airport locations in the database
     query = '''SELECT AirportLocation FROM Airport'''
     cursor.execute(query)
     return cursor.fetchall()
 
-def view_departureinformation(cursor, departureairport):
+def return_allairportIDs(cursor):
+    # Return all airport IDs which is used for validation routines
+    query = '''SELECT AirportID FROM Airport'''
+    cursor.execute(query)
+    return cursor.fetchall()
 
+def view_departureinformation(cursor, departureairport):
+    # Show all departure information based on the departure airport
     query = ''' SELECT
                 Airport.AirportStatus,
                 Flight.FlightID,
@@ -45,7 +55,7 @@ def view_departureinformation(cursor, departureairport):
     return cursor.fetchall()
 
 def view_arrivalinformation(cursor,arrivalairport):
-
+    # Show all arrival information based on the arrival airport
     query = ''' SELECT
                 Airport.AirportStatus,
                 Flight.FlightID,
@@ -63,3 +73,28 @@ def view_arrivalinformation(cursor,arrivalairport):
     cursor.execute(query, (arrivalairport,))
     rows = cursor.fetchall()
 
+def return_allaircraft(cursor):
+    # Return all aircraft IDs which is used for validation routines
+    query = '''SELECT AircraftID FROM Flight'''
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def return_allFlightIDs(cursor):
+    # Return all Flight IDs which is used for validation routines
+    query = '''SELECT FlightID FROM Flight'''
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def addnewFlight(cursor,FlightID,FlightStatus,DepartureTime,ArrivalTime,StartAirportID,EndAirportID,AircraftID):
+    # Add a new Flight based on User Input
+    query = '''INSERT INTO Flight(FlightID,FlightStatus,DepartureTime,ArrivalTime,StartAirportID, EndAirportID, AircraftID)
+    VALUES(?,?,?,?,?,?,?)'''
+    cursor.execute(query,(FlightID,FlightStatus,DepartureTime,ArrivalTime,StartAirportID, EndAirportID,AircraftID))
+
+    # Return the latest Flight Data so user can view the update made
+    query = '''SELECT FlightID, FlightStatus, DepartureTime, ArrivalTime,StartAirportID,EndAirportID, AircraftID FROM Flight'''
+    cursor.execute(query)
+    return cursor.fetchall()
+
+    
